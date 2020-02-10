732ccbf08a89cd2293a9343db43b6846cc22c0d5
ANES TEMANI <49069012+anes019@users.noreply.github.com>
2020-02-10 14:09:33 +0100

    anes

--- TWASALNI/src/Utilisateurs/UtilisateursBundle/Entity/Utilisateurs.php
+++ TWASALNI/src/Utilisateurs/UtilisateursBundle/Entity/Utilisateurs.php
@@ -19,6 +19,52 @@ class Utilisateurs extends BaseUser
      */
     protected $id;
 
+    /**
+
+     * @ORM\Column(type="string", length=255)
+
+     */
+    protected $nom;
+    /**
+
+     * @ORM\Column(type="string" , length=255)
+
+     */
+    protected $prenom;
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+
     public function __construct()
     {
         parent::__construct();

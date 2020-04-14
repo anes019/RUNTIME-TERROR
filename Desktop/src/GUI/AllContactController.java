a31f7b4eeb38d968f6a05bc8dc92f3cce45f6353
maurel237 <48739062+maurel237@users.noreply.github.com>
2020-04-14 09:31:30 -0500

    ...

--- Desktop/src/GUI/AllContactController.java
+++ Desktop/src/GUI/AllContactController.java
@@ -8,12 +8,18 @@ package GUI;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import desktop.Entite.Partenaire;
 import desktop.Entite.Reclammations;
+import desktop.Entite.ViewReclammation;
+import desktop.Service.ServiceFeedBack;
 import desktop.Service.ServiceReclammation;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -23,7 +29,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -40,6 +48,8 @@ public class AllContactController implements Initializable {
      */
     
     ServiceReclammation ServR = new ServiceReclammation();
+    
+    ServiceFeedBack ServF = new ServiceFeedBack();
     @FXML
     private VBox vbox;
     @FXML
@@ -69,23 +79,26 @@ public class AllContactController implements Initializable {
     @FXML
     private TableView<?> tableview;
     @FXML
-    private TableColumn<Reclammations, Integer> user;
+    private TableColumn<ViewReclammation, String> user;
     @FXML
-    private TableColumn<Reclammations, String> sujet;
+    private TableColumn<ViewReclammation, String> sujet;
     @FXML
-    private TableColumn<Reclammations, String> contenu;
+    private TableColumn<ViewReclammation, String> contenu;
     @FXML
-    private TableColumn<Reclammations, Date> date;
+    private TableColumn<ViewReclammation, Date> date;
     @FXML
     private JFXHamburger hamburger;
     @FXML
     private JFXDrawer draw;
     @FXML
-    private TableColumn<Reclammations, String> etat;
+    private TableColumn<ViewReclammation, String> etat;
     @FXML
     private TableColumn<?, ?> actions;
     @FXML
     private Label total;
+    @FXML
+    private TextField search;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         draw.setSidePane(vbox);
@@ -105,29 +118,64 @@ public class AllContactController implements Initializable {
             }
         });
         
-        DisplayAll();
+        try {
+            DisplayAll();
+        } catch (SQLException ex) {
+            Logger.getLogger(AllContactController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }    
-    
-    public void DisplayAll(){
+    public ArrayList<ViewReclammation> CreateTab() throws SQLException{
         ArrayList<Reclammations> TabR = ServR.FindAll();
-        int total = 0;
+        
+        ArrayList<ViewReclammation> TabA = new ArrayList<>();
         for(Reclammations r : TabR){
-            total++;
+            Partenaire P = ServF.FindUser(r.getUser());
+            TabA.add(new ViewReclammation(r.getId(), P.getNom()+" "+P.getPrenom(), r.getSujet(),r.getContenu(),""+r.getDate(),r.getEtat()));    
         }
-        ObservableList ViewRec = FXCollections.observableArrayList(TabR);
+        return TabA;
+    }
+    public void DisplayAll() throws SQLException{
+        
+        ArrayList<ViewReclammation> TabA = CreateTab();
+        ObservableList ViewRec = FXCollections.observableArrayList(TabA);
         tableview.setItems(ViewRec);
         user.setCellValueFactory(new PropertyValueFactory<>("user"));
         sujet.setCellValueFactory(new PropertyValueFactory<>("sujet"));
         contenu.setCellValueFactory(new PropertyValueFactory<>("contenu"));
         date.setCellValueFactory(new PropertyValueFactory<>("date"));
         etat.setCellValueFactory(new PropertyValueFactory<>("etat"));
-        this.total.setText(""+total);
+        this.total.setText(""+TabA.size());
     }
 
     
     @FXML
     private void handleClicks(ActionEvent event) {
+    }
+
+    @FXML
+    private void SearchAll(KeyEvent event) throws SQLException {
+        ArrayList<ViewReclammation> TabA = CreateTab();
+        ArrayList<ViewReclammation> TabB = new ArrayList<>();
+        for(ViewReclammation R : TabA){
+            if(R.getUser().toLowerCase().indexOf(search.getText().toLowerCase()) != -1
+                    || R.getContenu().toLowerCase().indexOf(search.getText().toLowerCase()) != -1
+                    || R.getSujet().toLowerCase().indexOf(search.getText().toLowerCase()) != -1
+                    ||R.getDate().toLowerCase().indexOf(search.getText().toLowerCase()) != -1
+                    ||R.getEtat().toLowerCase().indexOf(search.getText().toLowerCase()) != -1){
+                System.out.println(R.getUser());
+                TabB.add(new ViewReclammation(R.getId(), R.getUser(), R.getSujet(),R.getContenu(),""+R.getDate(),R.getEtat()));    
+        
+            }
+        }
+        ObservableList ViewRec = FXCollections.observableArrayList(TabB);
+        tableview.setItems(ViewRec);
+        user.setCellValueFactory(new PropertyValueFactory<>("user"));
+        sujet.setCellValueFactory(new PropertyValueFactory<>("sujet"));
+        contenu.setCellValueFactory(new PropertyValueFactory<>("contenu"));
+        date.setCellValueFactory(new PropertyValueFactory<>("date"));
+        etat.setCellValueFactory(new PropertyValueFactory<>("etat"));
+        this.total.setText(""+TabB.size());
     }
     
     
